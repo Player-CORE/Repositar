6eeb363acf64c83da12d4ababc70b08da83f97a6
Player CORE <player.core1@gmail.com>
2025-11-11 16:29:10 +0100

    Zmena

---
+++ MyLittlePonyFriendshipIsMagic/MyLittlePonyFriendshipIsMagic/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MyLittlePonyFriendshipIsMagic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberPeople = Convert.ToInt32(Console.ReadLine());
+            bool[,] matrix = new bool[NumberPeople, NumberPeople];
+            string inputData = Console.ReadLine();
+            string[] dataSplit = inputData.Split(" ");
+            for (int i = 0; i < dataSplit.Length; i++) 
+            {
+                string[] data = dataSplit[i].Split("-");
+                matrix[Convert.ToInt32(data[0]), Convert.ToInt32(data[1])] = true;
+            }
+
+        }
+    }
+}
