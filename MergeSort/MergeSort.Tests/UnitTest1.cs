69607411fcc73eac68b2be29cc184a92a59ac191
Player CORE <player.core1@gmail.com>
2026-03-24 16:38:18 +0100

    Posílám quicksort

---
+++ MergeSort/MergeSort.Tests/UnitTest1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using MergeSort;
+
+namespace MergeSort.Tests
+{
+    public class UnitTest1
+    {
+
+        [Fact]  // Tím označujeme, že jde o testovací metodu      
+
+        public void Merge_EqualLengthArrays_ReturnsMergedSortedArray()         // Naming convention pro testy: ClassName_FunctionName_ExpectedResult nebo FunctionName_TestSpecifics_ExpectedResult
+        {
+            // Arrange - nastavme vše co potřebujeme, aby mohla běžet testovaná funkce
+            int[] array = { 1, 3, 5, 2, 3, 6 };
+            int[] expectedArray = { 1, 2, 3, 3, 5, 6 };
+            int left = 0;
+            int right = array.Length - 1;
+            int middle = left + (right - left) / 2;
+
+            // Act - zavoláme testovanou funkci
+            MergeSortClass.Merge(array, left, middle, right);
+
+            // Assert - zkontrolujeme to, co nám funkce vrátila
+            Assert.Equal(expectedArray, array);
+        }
+
+        [Fact]  // Tím označujeme, že jde o testovací metodu      
+
+        public void SingleNumbah()         // Naming convention pro testy: ClassName_FunctionName_ExpectedResult nebo FunctionName_TestSpecifics_ExpectedResult
+        {
+            // Arrange - nastavme vše co potřebujeme, aby mohla běžet testovaná funkce
+            int[] array = { 1 };
+            int[] expectedArray = { 1 };
+            int left = 0;
+            int right = array.Length - 1;
+            int middle = left + (right - left) / 2;
+
+            // Act - zavoláme testovanou funkci
+            MergeSortClass.Merge(array, left, middle, right);
+
+            // Assert - zkontrolujeme to, co nám funkce vrátila
+            Assert.Equal(expectedArray, array);
+        }
+        [Fact]  // Tím označujeme, že jde o testovací metodu      
+
+        public void Negative()         // Naming convention pro testy: ClassName_FunctionName_ExpectedResult nebo FunctionName_TestSpecifics_ExpectedResult
+        {
+            // Arrange - nastavme vše co potřebujeme, aby mohla běžet testovaná funkce
+            int[] array = { -8, -2, -1, -97, -69, -67 };
+            int[] expectedArray = { -97, -69, -67, -8, -2, -1, };
+            int left = 0;
+            int right = array.Length - 1;
+            int middle = left + (right - left) / 2;
+
+            // Act - zavoláme testovanou funkci
+            MergeSortClass.Merge(array, left, middle, right);
+
+            // Assert - zkontrolujeme to, co nám funkce vrátila
+            Assert.Equal(expectedArray, array);
+        }
+        [Fact]  // Tím označujeme, že jde o testovací metodu      
+
+        public void AlreadySorted()         // Naming convention pro testy: ClassName_FunctionName_ExpectedResult nebo FunctionName_TestSpecifics_ExpectedResult
+        {
+            // Arrange - nastavme vše co potřebujeme, aby mohla běžet testovaná funkce
+            int[] array = { 1, 2, 3, 4, 5 };
+            int[] expectedArray = { 1, 2, 3, 4, 5 };
+            int left = 0;
+            int right = array.Length - 1;
+            int middle = left + (right - left) / 2;
+
+            // Act - zavoláme testovanou funkci
+            MergeSortClass.Merge(array, left, middle, right);
+
+            // Assert - zkontrolujeme to, co nám funkce vrátila
+            Assert.Equal(expectedArray, array);
+        }
+        [Fact]  // Tím označujeme, že jde o testovací metodu      
+
+        public void RepeatingValues()         // Naming convention pro testy: ClassName_FunctionName_ExpectedResult nebo FunctionName_TestSpecifics_ExpectedResult
+        {
+            // Arrange - nastavme vše co potřebujeme, aby mohla běžet testovaná funkce
+            int[] array = { 1, 2, 2, 9, 1, 8, 8 };
+            int[] expectedArray = { 1, 1, 2, 2, 8, 8, 9};
+            int left = 0;
+            int right = array.Length - 1;
+            int middle = left + (right - left) / 2;
+
+            // Act - zavoláme testovanou funkci
+            MergeSortClass.Merge(array, left, middle, right);
+
+            // Assert - zkontrolujeme to, co nám funkce vrátila
+            Assert.Equal(expectedArray, array);
+        }
+        [Fact]  // Tím označujeme, že jde o testovací metodu      
+
+        public void AlmostSorted()         // Naming convention pro testy: ClassName_FunctionName_ExpectedResult nebo FunctionName_TestSpecifics_ExpectedResult
+        {
+            // Arrange - nastavme vše co potřebujeme, aby mohla běžet testovaná funkce
+            int[] array = { 1, 2, 87, 3, 4, 5 };
+            int[] expectedArray = { 1, 2, 3, 4, 5, 87 };
+            int left = 0;
+            int right = array.Length - 1;
+            int middle = left + (right - left) / 2;
+
+            // Act - zavoláme testovanou funkci
+            MergeSortClass.Merge(array, left, middle, right);
+
+            // Assert - zkontrolujeme to, co nám funkce vrátila
+            Assert.Equal(expectedArray, array);
+        }
+    }
+}
